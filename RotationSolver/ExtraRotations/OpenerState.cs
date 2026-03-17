44bdc2e7b07b95fe4f8d967849ada75dad50de7e
BrakusTapus <x_aj1993@live.nl>
2026-03-17 19:57:21 +0100

    added OpenerState

---
+++ RotationSolver/ExtraRotations/OpenerState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RotationSolver.ExtraRotations;
+
+public enum OpenerState
+{
+    OpenerNotReady,
+    OpenerReady,
+    InOpener,
+    OpenerFinished,
+    FailedOpener
+}
