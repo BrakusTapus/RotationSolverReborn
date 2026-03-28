4cd8037c23d1a174117937ef3643079d0dffa7c0
BrakusTapus <x_aj1993@live.nl>
2026-03-29 00:19:14 +0100

    meh

--- RotationSolver/ExtraRotations/Ranged/KirboMCH.cs
+++ RotationSolver/ExtraRotations/Ranged/KirboMCH.cs
@@ -1,9 +1,12 @@
+using Dalamud.Game.ClientState.JobGauge.Types;
+using ECommons.DalamudServices;
+using RotationSolver.Commands;
 using RotationSolver.ExtraRotations;
 
 namespace RotationSolver.ExtraRotations;
 
 [ExtraRotation]
-[Rotation("Kirbo - Oh boy..", CombatType.PvE, GameVersion = "7.5", Description = "Oh boy....", Disabled = true)]
+[Rotation("Kirbo - Oh boy..", CombatType.PvE, GameVersion = "7.45", Description = "Oh boy....", Disabled = true)]
 public sealed class KirboMCH : MachinistRotation
 {
     protected override IAction? CountDownAction(float remainTime)
@@ -35,5 +38,62 @@ public sealed class KirboMCH : MachinistRotation
     {
 
     }
+
+    private static MCHGauge Gauge => Svc.Gauges.Get<MCHGauge>();
+    private static float SummonTimeRemaining => Gauge.SummonTimeRemaining / 1000f;
+
+    #region Config
+    [RotationConfig(CombatType.PvE, Name = "Cancel Auto state if early pull")]
+    public bool EarlyPullCancelAuto { get; set; } = true;
+
+    [Range(0.1f, 5, ConfigUnitType.Seconds, 0.001f)]
+    [RotationConfig(CombatType.PvE, Name = "If combat started when countdown time > cancel timer")]
+    public float EarlyPullCancelAutoTimer { get; set; } = 1.0f;
+    #endregion
+
+    #region State
+    private delegate bool ActionExecutor(out IAction? act);
+    private sealed class StepDef
+    {
+        public Func<bool> When;
+        public ActionExecutor Use;
+        public StepDef(Func<bool> when, ActionExecutor use) { When = when; Use = use; }
+    }
+    private sealed class SequenceDef
+    {
+        public Func<bool> ShouldBeActive;   // Is this sequence currently valid/running?
+        public Func<bool> IsComplete;       // Has the whole sequence finished?
+        public StepDef[] Steps;             // Ordered list of actions to take
+
+        public SequenceDef(Func<bool> shouldBeActive, Func<bool> isComplete, params StepDef[] steps)
+        {
+            ShouldBeActive = shouldBeActive;
+            IsComplete = isComplete;
+            Steps = steps;
+        }
+    }
+    private bool ExecuteSequence(SequenceDef seq, out IAction? act)
+    {
+        act = null;
+        if (!seq.ShouldBeActive() || seq.IsComplete()) return false;
+
+        foreach (var step in seq.Steps)
+        {
+            if (step.When()) return step.Use(out act);
+        }
+
+        return false;
+    }
+    private static bool EnoughWeaveTime => WeaponRemain > DataCenter.CalculatedActionAhead && WeaponRemain < WeaponTotal;
+    private static float LateWeaveWindow => WeaponTotal * 0.4f;
+    private static bool CanLateWeave => WeaponRemain <= LateWeaveWindow && EnoughWeaveTime;
+    #endregion
+
+    #region Display
+    public override void DisplayRotationStatus()
+    {
+        ImGui.Text($"sample text");
+    }
+    #endregion
 
 }
